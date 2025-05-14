acba5e1ec12221781acb8aaaaa7b22fa7cc1400d
anderpups <anderpups@gmail.com>
2025-05-14 19:43:40 -0400

    Update getUniversalHotelRates.py

--- getUniversalHotelRates.py
+++ getUniversalHotelRates.py
@@ -1,19 +1,55 @@
 import requests
+import json
+import pandas
+import pprint
+import time
+import random
+from datetime import datetime, date, timedelta
 from bs4 import BeautifulSoup
 
-def hotel_data(date):
-  html = requests.get('https://reservations.universalorlando.com/ibe/default.aspx?hgID=641&langID=1&checkin='+ date +'&nights=1&rooms=1&adults=2&children=0&promo=aph&iata=&group=&hotels=&ada=')
-  print(html)
-  # soup = BeautifulSoup(html, 'html.parser')
-  # hotels_html = soup.find('section', id='cnWsResultHotels')
-  # hotels = hotels_html.find_all('div', class_='ws-property-item')
-  # global date_info
-  # date_info = {'date': date, hotels:[]}
-  # for hotel in hotels:
-  #   hotel_name = hotel.find('div', class_='ws-property-title').h1.a.text
-  #   hotel_price = hotel.find('div', class_='ws-property-price').span.text
-  #   date_info.hotels.append[{"name": hotel_name, "price": hotel_price}]
+today = datetime.now()
+tomorrow = today + timedelta(days=1)
+date_range = pandas.date_range(start=tomorrow, periods=2)
+hotel_info = []
 
-hotel_data('9/25/2025')
+def get_data_for_date(date):
+  params = {
+    'hgID': 641,
+    'langID': 1,
+    'checkin': date,
+    'nights': 1,
+    'adults': 2,
+    'children': 0,
+    'promo': 'aph',
+    'iata': '',
+    'group': '',
+    'hotels': '',
+    'ada': ''
+  }
+  html = requests.get('https://reservations.universalorlando.com/ibe/default.aspx', params=params)
+  soup = BeautifulSoup(html.text, 'html.parser')
+  hotels_html = soup.find('section', id='cnWsResultHotels')
+  hotels = hotels_html.find_all('div', class_='ws-property-item')
+  global date_info
+  date_info = {"date": date}
+  date_info["hotels"] = []
+  for hotel in hotels:
+    hotel_name = hotel.find('div', class_='ws-property-title').h1.a.text
+    hotel_price = hotel.find('div', class_='ws-property-price').span.text
+    date_info['hotels'].append({"name": hotel_name, "price": hotel_price})
 
-# print(date_info)
+
+for date in date_range:
+  print('Grabbing hotel price data for ' + str(date.strftime('%m/%d/%Y')))
+  get_data_for_date(date.strftime('%m/%d/%Y'))
+  hotel_info.append(date_info)
+  random_ms = random.randint(1000,60000)
+  random_second = random_ms / 1000
+  random_minute = random_second /60
+  print('Sleeping for ' + str(round(random_minute, 2)) + ' minutes')
+  time.sleep(random_second)
+
+print(hotel_info)
+
+with open("hotel_info.json", "wt") as file:
+  pprint.pprint(json.dumps(hotel_info), stream=file)
