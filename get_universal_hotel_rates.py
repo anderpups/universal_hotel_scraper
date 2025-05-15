6902f52dc3ffbbf53ac82f3f2d4152e97c91b09d
anderpups <anderpups@gmail.com>
2025-05-14 22:06:00 -0400

    done for day

---
+++ get_universal_hotel_rates.py
@@ -0,0 +1,80 @@
+import requests
+import json
+import pandas
+import time
+import random
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+
+today = datetime.now()
+tomorrow = today + timedelta(days=1)
+
+## Generate a list of 365 random dates starting from tomorrow
+date_range = list(pandas.date_range(start=tomorrow, periods=365, freq='D'))
+random.shuffle(date_range)
+
+hotel_info = []
+
+## Function to fetch hotel data for a specific date
+## and return a list of dictionaries with hotel name, price, and date
+## The function takes a date string in the format MM/DD/YYYY
+def get_data_for_date(date):
+  """
+  Fetch hotel data for a specific date string (MM/DD/YYYY).
+  Returns a list of dictionaries with hotel name price and date.
+  """
+  ## Set the query parameters for the web request
+  params = {
+    'hgID': 641,
+    'langID': 1,
+    'checkin': date,
+    'nights': 1,
+    'adults': 2,
+    'children': 0,
+    'promo': 'aph',
+    'iata': '',
+    'group': '',
+    'hotels': '',
+    'ada': ''
+  }
+  dt = datetime.strptime(date, '%m/%d/%Y')
+  try:
+    ## Make the web request to the Universal Orlando website
+    response = requests.get('https://reservations.universalorlando.com/ibe/default.aspx', params=params, timeout=15)
+    ## Check if the response is successful
+    response.raise_for_status()
+    ## Parse the response content using BeautifulSoup
+    ## and extract the hotel price info
+    soup = BeautifulSoup(response.text, 'html.parser')
+    hotels_html = soup.find('section', id='cnWsResultHotels')
+    if not hotels_html:
+      return []
+    hotels = hotels_html.find_all('div', class_='ws-property-item')
+    hotels_list = []
+    for hotel in hotels:
+      try:
+        hotel_name = hotel.find('div', class_='ws-property-title').h1.a.text
+        hotel_price = hotel.find('div', class_='ws-property-price').span.text
+        hotels_list.append({"name": hotel_name, "price": hotel_price,"date": date})
+      except Exception as e:
+        print(f"Error parsing hotel info: {e}")
+    return hotels_list
+  except Exception as e:
+    print(f"Error fetching data for {date}: {e}")
+    return []
+
+## Loop through the date range and fetch hotel data
+for date in date_range:
+  date_str = date.strftime('%m/%d/%Y')
+  date_info = get_data_for_date(date_str)
+  hotel_info.extend(date_info)
+  print(f"Found {len(date_info)} hotels available for {date_str}")
+  sleep_seconds = random.uniform(1, 6)
+  time.sleep(sleep_seconds)
+
+## create filename with today's date
+filename = f"hotel_info-{today.strftime('%Y%m%d')}.json"
+
+## Write the data to a JSON file
+with open(filename, "w") as file:
+  json.dump(hotel_info, file, indent=2)
---
+++ process_hotel_rate_info.py
@@ -0,0 +1,16 @@
+import glob
+import json
+import os
+
+data_folder = "data"
+all_data = []
+
+# Find all JSON files in the data folder
+json_files = glob.glob(os.path.join(data_folder, "*.json"))
+
+for file_path in json_files:
+    with open(file_path, "r") as f:
+        data = json.load(f)
+        all_data.extend(data)
+
+print(f"Loaded {len(all_data)} records from {len(json_files)} files.")
