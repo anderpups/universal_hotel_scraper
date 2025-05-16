aa0585e7f97b5e85dfbb23e0b31b20e3204ee467
anderpups <anderpups@gmail.com>
2025-05-15 22:05:31 -0400

    done for the day

--- process_hotel_rate_info.py
+++ process_hotel_rate_info.py
@@ -9,6 +9,13 @@ all_data = []
 # Find all JSON files in the data folder
 json_files = glob.glob(os.path.join(data_folder, "*.json"))
 
+def format_price(price):
+    """Format the price to include a dollar sign """
+    try:
+        return f"${int(price)}"
+    except ValueError:
+        return "N/A"
+
 for file_path in json_files:
     with open(file_path, "r") as f:
         data = json.load(f)
@@ -17,4 +24,11 @@ for file_path in json_files:
 
     pivot_df = df.pivot_table(index='date', columns='name', values='price')
     
-    pivot_df.to_html(f"{data_folder}/hotel_info-{gather_date}.html")
+    pivot_df.index = pandas.to_datetime(pivot_df.index, format='%m/%d/%Y')
+    pivot_df = pivot_df.sort_index()
+    pivot_df.index = pivot_df.index.strftime('%m/%d/%y')
+
+    for col in pivot_df.columns:
+        pivot_df[col] = pivot_df[col].apply(format_price)
+    
+    pivot_df.to_html(f"{data_folder}/hotel_info-{gather_date}.md", index_names=False)
