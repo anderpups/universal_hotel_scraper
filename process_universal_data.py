5cd1540037923284665c27d069d499ed9b24039e
anderpups <anderpups@gmail.com>
2025-12-15 18:08:12 -0500

    Update next trip date to January 29, 2025

--- process_universal_data.py
+++ process_universal_data.py
@@ -10,7 +10,7 @@ from itertools import groupby
 from datetime import datetime, timedelta
 from jinja2 import Environment, FileSystemLoader
 
-next_trip_date = 'Dec 12, 2025 20:00:00'
+next_trip_date = 'Jan 29, 2025 09:00:00'
 
 html_folder = 'html'
 data_folder = f'{html_folder}/data'
