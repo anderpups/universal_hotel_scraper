53ea2678cd50c3e6f7c1f267b530edf64682dc2c
anderpups <anderpups@gmail.com>
2025-12-30 08:16:57 -0500

    fix html render

--- process_universal_data.py
+++ process_universal_data.py
@@ -14,10 +14,12 @@ data_folder = f'{html_folder}/data'
 index_html_file_path = os.path.join(html_folder, "index.html")
 environment = Environment(loader=FileSystemLoader("templates/"))
 hotel_prices_template = environment.get_template("hotel_prices.html.j2")
+hotel_prices_html_file_path = os.path.join(html_folder, "hotel_prices.html")
 index_template = environment.get_template("index.html.j2")
 
 ## Get todday
 today = datetime.now(ZoneInfo("America/New_York"))
+print(f"Today's date: {today.strftime('%Y-%m-%d')}")
 ## Get tomorrow
 tomorrow = today + timedelta(days=1)
 end_date = today + timedelta(days=365)
@@ -27,15 +29,17 @@ index_html = index_template.render(
     next_trip_date=next_trip_date
     )
 
+# Write to index.html
+with open(index_html_file_path, "w") as f:
+    f.write(index_html)
+
 hotel_prices_html = hotel_prices_template.render(
     today_date=today.strftime('%Y-%m-%d'),
     end_date=end_date.strftime('%Y-%m-%d')
     )
 
-# Write to index.html
-with open(index_html_file_path, "w") as f:
-    f.write(index_html)
-print(f"Generated HTML for index.html")
+with open(hotel_prices_html_file_path, "w") as f:
+    f.write(hotel_prices_html)
 
 data_location_list = ''
 
